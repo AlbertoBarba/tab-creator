bfc90bbf461cd38b8550437fe57d4805512c6de8
Alberto Barba <alberto.barba@live.com>
2026-05-23 11:00:00 +0000

    feat: integrate useKeyboardEditor hook into TabCanvas

--- src/components/Editor/TabCanvas.tsx
+++ src/components/Editor/TabCanvas.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useTabStore } from '../../store/useTabStore';
 import { MeasureRenderer } from './Measure';
+import { useKeyboardEditor } from '../../hooks/useKeyboardEditor';
 
 export const TabCanvas: React.FC = () => {
   const song = useTabStore((state) => state.song);
+
+  // Register keyboard listeners
+  useKeyboardEditor();
 
   return (
     <div className="p-8 bg-white overflow-auto h-full">
